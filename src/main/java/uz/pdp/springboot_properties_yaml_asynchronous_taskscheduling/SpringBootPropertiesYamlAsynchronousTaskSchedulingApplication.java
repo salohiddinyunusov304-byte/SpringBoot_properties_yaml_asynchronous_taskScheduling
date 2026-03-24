c8e76655f653e208fb74b068cf74789068d6e4d3
Salohiddin Yunusov <salohiddinyunusov304@gmail.com>
2026-03-24 16:21:31 +0500

    springboot

---
+++ src/main/java/uz/pdp/springboot_properties_yaml_asynchronous_taskscheduling/SpringBootPropertiesYamlAsynchronousTaskSchedulingApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springboot_properties_yaml_asynchronous_taskscheduling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPropertiesYamlAsynchronousTaskSchedulingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPropertiesYamlAsynchronousTaskSchedulingApplication.class, args);
+    }
+
+}
