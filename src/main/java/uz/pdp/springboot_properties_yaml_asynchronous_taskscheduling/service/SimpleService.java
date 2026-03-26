289fa18c71630546b6e9f41f31a89d4a4c3606a6
Salohiddin Yunusov <salohiddinyunusov304@gmail.com>
2026-03-26 15:22:32 +0500

    Spring boot task scheduling is done

--- src/main/java/uz/pdp/springboot_properties_yaml_asynchronous_taskscheduling/service/SimpleService.java
+++ src/main/java/uz/pdp/springboot_properties_yaml_asynchronous_taskscheduling/service/SimpleService.java
@@ -1,7 +1,6 @@
 package uz.pdp.springboot_properties_yaml_asynchronous_taskscheduling.service;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
